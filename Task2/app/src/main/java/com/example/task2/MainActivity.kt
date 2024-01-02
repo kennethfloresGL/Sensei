06d83fc0c1f9ee82c28610e9fc14047e3d5c4b7e
Kenneth Flores <kenneth.flores@gorillalogic.com>
2024-01-02 11:42:13 -0600

    Mi Primer Commit
    Yei la la la la

---
+++ Task2/app/src/main/java/com/example/task2/MainActivity.kt
@@ -0,0 +1,245 @@
+package com.example.task2
+
+import android.graphics.fonts.FontFamily
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.task2.ui.theme.Task2Theme
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Task2Theme {
+                var newName by remember { mutableStateOf("") }
+
+                Column (
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(colorResource(id = R.color.background))
+                ) {
+                    Text(
+                        text = "facebook",
+                        modifier = Modifier
+                            .padding(top = 200.dp),
+                        color = Color.White,
+                        fontSize = 50.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+
+                    // Email or phone number
+                    TextField(
+                        value = newName,
+                        onValueChange = { newName = it },
+                        label = { Text("Email or phone number") },
+                        modifier = Modifier
+                            .padding(15.dp)
+                            .fillMaxWidth()
+                            .background(Color.White)
+                    )
+
+                    // Password
+                    TextField(
+                        value = newName,
+                        onValueChange = { newName = it },
+                        label = { Text("Password") },
+                        modifier = Modifier
+                            .padding(15.dp)
+                            .fillMaxWidth()
+                            .background(Color.White)
+                    )
+
+                    // Button
+                    Button(
+                        onClick = {
+                            // Your login logic here
+                        },
+                        colors = ButtonDefaults.buttonColors(colorResource(R.color.purple_200)),
+                        modifier = Modifier
+                            .padding(15.dp)
+                            .fillMaxWidth()
+                    ) {
+                        Text("Log In", color = Color.White)
+                    }
+
+                    // Sign Up for Facebook
+                    ClickableText(
+                        text = buildAnnotatedString {
+                            withStyle(style = SpanStyle(color = Color.White)) {
+                                append("Sign Up for Facebook")
+                            }
+                            addStringAnnotation(
+                                tag = "URL",
+                                annotation = "https://www.facebook.com/signup",
+                                start = 0,
+                                end = length
+                            )
+                        },
+                        onClick = {// Your logic here
+                        },
+                        modifier = Modifier.padding(top = 100.dp)
+                    )
+
+                    // Need Help?
+                    ClickableText(
+                        text = buildAnnotatedString {
+                            withStyle(style = SpanStyle(color = Color.White)) {
+                                append("Need Help?")
+                            }
+                            addStringAnnotation(
+                                tag = "URL",
+                                annotation = "https://www.facebook.com/help",
+                                start = 0,
+                                end = length
+                            )
+                        },
+                        onClick = {// Your logic here
+                        },
+                        modifier = Modifier.padding(16.dp)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@RequiresApi(Build.VERSION_CODES.Q)
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Task2Theme {
+        var newName by remember { mutableStateOf("") }
+
+        Column (
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .background(colorResource(id = R.color.background))
+        ) {
+            Text(
+                text = "facebook",
+                modifier = Modifier
+                    .padding(top = 200.dp),
+                    color = Color.White,
+                    fontSize = 50.sp,
+                    fontWeight = FontWeight.Bold
+            )
+
+            // Email or phone number
+            TextField(
+                value = newName,
+                onValueChange = { newName = it },
+                label = { Text("Email or phone number") },
+                modifier = Modifier
+                    .padding(15.dp)
+                    .fillMaxWidth()
+                    .background(Color.White)
+            )
+
+            // Password
+            TextField(
+                value = newName,
+                onValueChange = { newName = it },
+                label = { Text("Password") },
+                modifier = Modifier
+                    .padding(15.dp)
+                    .fillMaxWidth()
+                    .background(Color.White)
+            )
+
+            Button(
+                onClick = {
+                    // Your login logic here
+                },
+                colors = ButtonDefaults.buttonColors(colorResource(R.color.purple_200)),
+                modifier = Modifier
+                    .padding(15.dp)
+                    .fillMaxWidth()
+            ) {
+                Text("Log In", color = Color.White)
+            }
+
+            // Sign Up for Facebook
+            ClickableText(
+                text = buildAnnotatedString {
+                    withStyle(style = SpanStyle(color = Color.White)) {
+                        append("Sign Up for Facebook")
+                    }
+                    addStringAnnotation(
+                        tag = "URL",
+                        annotation = "https://www.facebook.com/signup",
+                        start = 0,
+                        end = length
+                    )
+                },
+                onClick = {// Your logic here
+                },
+                modifier = Modifier.padding(top = 100.dp)
+            )
+
+            // Need Help?
+            ClickableText(
+                text = buildAnnotatedString {
+                    withStyle(style = SpanStyle(color = Color.White)) {
+                        append("Need Help?")
+                    }
+                    addStringAnnotation(
+                        tag = "URL",
+                        annotation = "https://www.facebook.com/help",
+                        start = 0,
+                        end = length
+                    )
+                },
+                onClick = {// Your logic here
+                },
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+    }
+}
+
